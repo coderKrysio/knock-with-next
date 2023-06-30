b3f3549e9fdc36c886470026aadf1b3ca5836169
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-01 00:39:06 +0530

    added message form

--- components/DisplayUser.tsx
+++ components/DisplayUser.tsx
@@ -4,10 +4,24 @@ import { useEffect, useState } from "react"
 const DisplayUser = ({
     userData
 }:any) => {
+    const [message, setMessage] = useState("");
+    const [showToast, setShowToast] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+
     const [messageList,setMessageList] = useState<any>({
         items: [],
         pageInfo: {}
     });
+
+    const onSubmit = async (e: any) => {
+        e.preventDefault();
+        setIsLoading(true);
+        //await notify({ message, showToast, userData.cuid, tenant });
+        setIsLoading(false);
+
+        e.target.reset();
+    };
+    
     useEffect(() => {
         KnockAPI.getMessages(userData.cuid)
         .then((res: any) => {
@@ -28,6 +42,37 @@ const DisplayUser = ({
             <p>User Name: {userData.name}</p>
             <p>Messages: {messageList.items.length}</p>
             {/* <p>{messageList.items}</p> */}
+            <br />
+
+            <p>Send Message</p>
+
+            <form
+            onSubmit={onSubmit}
+            >
+                <input
+                type="text"
+                placeholder="type message to send"
+                className="placeholder:whitespace-normal text-left border-[1px] border-black my-2 p-2 rounded-lg"
+                onChange={(e) => setMessage(e.target.value)}
+                /> 
+                
+                <br />
+
+                <input
+                type="checkbox"
+                name="show-toast"
+                checked={showToast}
+                onChange={(e) => setShowToast(e.target.checked)}
+                />
+                <label htmlFor="show-toast">Show Toast</label>
+                
+                <br />
+
+                <button
+                disabled={message === ""}
+                className="border-2 border-black rounded-lg p-2 my-2"
+                >Send</button>
+            </form>
         </div>
     )
 }
