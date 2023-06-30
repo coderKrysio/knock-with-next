8bf0b259f9017f5e0ab30af44d31b0ea88a53b4c
Anushree <anushreejaiswal.srn@gmail.com>
2023-06-30 21:05:55 +0530

    message endpoint done successfully

--- components/DisplayUser.tsx
+++ components/DisplayUser.tsx
@@ -1,11 +1,32 @@
+import { KnockAPI } from "@/lib/knockapi"
+import { useEffect, useState } from "react"
+
 const DisplayUser = ({
     userData
 }:any) => {
+    const [messageList,setMessageList] = useState<any>({
+        items: [],
+        pageInfo: {}
+    });
+    useEffect(() => {
+        KnockAPI.getMessages(userData.cuid)
+        .then((res: any) => {
+            setMessageList((prev: any) => ({
+                ...prev,
+                items: res.items,
+                pageInfo: res.pageInfo,
+            }))
+            console.log(res)
+        })
+        .catch((err: any) => console.log(err))
+    }, [])
+
     return (
         <div
         className="border-2 border-black p-10 rounded-lg h-fit "
         >
             <p>User Name: {userData.name}</p>
+            <p>Messages: {messageList.items.length}</p>
         </div>
     )
 }
--- lib/knockapi.ts
+++ lib/knockapi.ts
@@ -6,13 +6,19 @@ export const KnockAPI = {
         return await knock.users.identify(userData.cuid, {
             name: userData.name,
             email: userData.email,
-        }).catch((res: any) => console.log(res))
+        }).catch((err: any) => console.log(err))
     },
 
     getUser:async (userId: any) => {
         return await knock.users.get(userId)
-        .catch((res: any) => console.log(res))
+        .catch((err: any) => console.log(err))
     },
 
-
+    getMessages: async (userId: any) => {
+        return await knock.users.getMessages(userId,
+            {
+                page_size: 5,
+            }
+        )
+    }
 }
