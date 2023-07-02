003f286c9161755dff87633b1d315c9325edf3f4
Anushree <anushreejaiswal.srn@gmail.com>
2023-07-02 16:21:21 +0530

    installed prettier

--- components/DisplayUser.tsx
+++ components/DisplayUser.tsx
@@ -1,58 +1,53 @@
-import { KnockAPI } from "@/lib/knockapi"
-import { useState } from "react"
+import { KnockAPI } from "@/lib/knockapi";
+import { useState } from "react";
 import NotificationFeed from "./NotificationFeed";
 import { KnockFeedProvider } from "@knocklabs/react-notification-feed";
 
-const DisplayUser = ({
-    userData,
-    recipientId
-}:any) => {
-    const [message, setMessage] = useState("");
+const DisplayUser = ({ userData, recipientId }: any) => {
+  const [message, setMessage] = useState("");
 
-    const onSubmit = async (e: any) => {
-        e.preventDefault();
-        KnockAPI.triggerWorkflow(userData.cuid, recipientId ,message);
-        e.target.reset();
-    };
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    KnockAPI.triggerWorkflow(userData.cuid, recipientId, message);
+    e.target.reset();
+  };
 
-    return (
-        <KnockFeedProvider
-        userId={userData.cuid}
-        apiKey={process.env.NEXT_PUBLIC_KNOCK_PUBLIC_API_KEY ?? ""}
-        feedId={process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID ?? ""}
-        >
-            <div
-            className="flex flex-col border-2 border-black p-10 rounded-lg h-screen w-full"
-            >
-                <div
-                className="flex items-center ml-auto"
-                ><NotificationFeed /></div>
-                
-                <p>User Name: {userData.name}</p>
-                <br />
+  return (
+    <KnockFeedProvider
+      userId={userData.cuid}
+      apiKey={process.env.NEXT_PUBLIC_KNOCK_PUBLIC_API_KEY ?? ""}
+      feedId={process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID ?? ""}
+    >
+      <div className="flex flex-col border-2 border-black p-10 rounded-lg h-screen w-full">
+        <div className="flex items-center ml-auto">
+          <NotificationFeed />
+        </div>
 
-                <p>Send Message</p>
+        <p>User Name: {userData.name}</p>
+        <br />
 
-                <form
-                onSubmit={onSubmit}
-                >
-                    <input
-                    type="text"
-                    placeholder="type message to send"
-                    className="placeholder:whitespace-normal text-left border-[1px] border-black my-2 p-2 rounded-lg"
-                    onChange={(e) => setMessage(e.target.value)}
-                    /> 
-                    
-                    <br />
+        <p>Send Message</p>
 
-                    <button
-                    disabled={message === ""}
-                    className="border-2 border-black rounded-lg p-2 my-2"
-                    >Send</button>
-                </form>
-            </div>
-        </KnockFeedProvider>
-    )
-}
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="type message to send"
+            className="placeholder:whitespace-normal text-left border-[1px] border-black my-2 p-2 rounded-lg"
+            onChange={(e) => setMessage(e.target.value)}
+          />
 
-export default DisplayUser
+          <br />
+
+          <button
+            disabled={message === ""}
+            className="border-2 border-black rounded-lg p-2 my-2"
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    </KnockFeedProvider>
+  );
+};
+
+export default DisplayUser;
--- lib/knockapi.ts
+++ lib/knockapi.ts
@@ -2,31 +2,30 @@ const { Knock } = require("@knocklabs/node");
 const knock = new Knock(process.env.NEXT_PUBLIC_KNOCK_API_KEY);
 
 export const KnockAPI = {
-    setIdentify: async (userData: any) => {
-        return await knock.users.identify(userData.cuid, {
-            name: userData.name,
-            email: userData.email,
-        }).catch((err: any) => console.log(err))
-    },
+  setIdentify: async (userData: any) => {
+    return await knock.users
+      .identify(userData.cuid, {
+        name: userData.name,
+        email: userData.email,
+      })
+      .catch((err: any) => console.log(err));
+  },
 
-    getUser: async (userId: any) => {
-        return await knock.users.get(userId)
-        .catch((err: any) => console.log(err))
-    },
+  getUser: async (userId: any) => {
+    return await knock.users.get(userId).catch((err: any) => console.log(err));
+  },
 
-    getMessages: async (userId: any) => {
-        return await knock.users.getMessages(userId,
-            {
-                page_size: 5,
-            }
-        )
-    },
+  getMessages: async (userId: any) => {
+    return await knock.users.getMessages(userId, {
+      page_size: 5,
+    });
+  },
 
-    triggerWorkflow: async (senderId: any, recipientId: any, message: any) => {
-        return await knock.workflows.trigger("test", {
-            recipients: [recipientId],
-            data: {  message },
-            actor: senderId,
-        });
-    }
-}
+  triggerWorkflow: async (senderId: any, recipientId: any, message: any) => {
+    return await knock.workflows.trigger("test", {
+      recipients: [recipientId],
+      data: { message },
+      actor: senderId,
+    });
+  },
+};
